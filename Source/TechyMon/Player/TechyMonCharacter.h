d1317aa99402d1605d8b523d4e72f9bc37ad5af5
Thomas Rondalli <thomas.rondalli@gmail.com>
2026-06-13 16:59:52 +0200

    feat: add TechyMonCharacter header

---
+++ Source/TechyMon/Player/TechyMonCharacter.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "InputActionValue.h"
+#include "TechyMonCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UPaperFlipbook;
+class UInputAction;
+
+UENUM(BlueprintType)
+enum class EFacingDirection : uint8
+{
+    Down,
+    Up,
+    Left,
+    Right
+};
+
+UCLASS()
+class TECHYMON_API ATechyMonCharacter : public APaperCharacter
+{
+    GENERATED_BODY()
+
+public:
+    ATechyMonCharacter();
+
+protected:
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<USpringArmComponent> SpringArm;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UCameraComponent> Camera;
+
+    UPROPERTY(EditAnywhere, Category = "Sprites")
+    TObjectPtr<UPaperFlipbook> FlipbookDown;
+
+    UPROPERTY(EditAnywhere, Category = "Sprites")
+    TObjectPtr<UPaperFlipbook> FlipbookUp;
+
+    UPROPERTY(EditAnywhere, Category = "Sprites")
+    TObjectPtr<UPaperFlipbook> FlipbookLeft;
+
+    UPROPERTY(EditAnywhere, Category = "Sprites")
+    TObjectPtr<UPaperFlipbook> FlipbookRight;
+
+    UPROPERTY(EditAnywhere, Category = "Input")
+    TObjectPtr<UInputAction> MoveAction;
+
+    EFacingDirection CurrentFacing = EFacingDirection::Down;
+    FVector2D PreviousInput = FVector2D::ZeroVector;
+
+    enum class ELastAxis : uint8 { None, Horizontal, Vertical };
+    ELastAxis LastPressedAxis = ELastAxis::None;
+
+    void HandleMove(const FInputActionValue& Value);
+    UPaperFlipbook* FlipbookForDirection() const;
+};
